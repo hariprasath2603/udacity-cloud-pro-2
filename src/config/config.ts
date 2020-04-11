3628c6ffa489ca16150f0529b2b44d506ccd3b12
hariprasath2603 <hariprasath2603@gmail.com>
2020-04-11 21:02:15 +0530

    new inital commit

---
+++ src/config/config.ts
@@ -0,0 +1,19 @@
+export const config = {
+  "dev": {
+    "username": process.env.username,
+    "password": process.env.username,
+    "database": process.env.username,
+    "host":  process.env.host,
+    "dialect": "postgres",
+    "aws_region": "us-east-2",
+    "aws_profile": "default",
+    "aws_media_bucket": process.env.bucket
+  },
+  "prod": {
+    "username": process.env.username,
+    "password": process.env.username,
+    "database": process.env.username,
+    "host": process.env.host,
+    "dialect": "postgres"
+  }
+}
